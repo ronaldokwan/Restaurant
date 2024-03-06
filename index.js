fdffbb03e266abd8e620382a7ac5332adf4db62a
Ronaldo Kwan <ronaldokwan17@gmail.com>
2024-03-06 16:45:23 +0700

    cors

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const router = require("./routers");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
